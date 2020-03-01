1a79d509dcbe322cacecadeedcf369449932fa62
emifervi <emifervi@hotmail.com>
2020-03-01 17:56:34 -0600

    correct mod 1, finis mod 2 topics 1-4

--- cursos/finanzas-personales/assets/js/quizes/modulo1-autoevaluacion.js
+++ cursos/finanzas-personales/assets/js/quizes/modulo1-autoevaluacion.js
@@ -1,9 +1,10 @@
 let numPreguntas = $(".form-group").length;
 
 $("#evaluar").click( _ => {
-    if($("input[type='radio']:checked").length === numPreguntas) {
-        if($(".answer").hasClass("answer")) {
-            $(".answer").removeClass("answer");
-        }
+    let answers = $("input[type='radio']:checked");
+    if(answers.length === numPreguntas) {
+        $(".hidden").removeClass("hidden")
+    } else {
+        alert("Es necesario responder todas las preguntas.")
     }
 });
