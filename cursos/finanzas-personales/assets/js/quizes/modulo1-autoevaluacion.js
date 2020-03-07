a18a09f2ec699d2f2d6a6814c6557fb21b563eb7
emifervi <emifervi@hotmail.com>
2020-03-07 15:51:46 -0600

    finish mod_5 and correct quiz script

--- cursos/finanzas-personales/assets/js/quizes/modulo1-autoevaluacion.js
+++ cursos/finanzas-personales/assets/js/quizes/modulo1-autoevaluacion.js
@@ -1,9 +1,10 @@
 let numPreguntas = $(".form-group").length;
 
 $("#evaluar").click( _ => {
+    $("div.col-11 > span").addClass("hidden");
     let answers = $("input[type='radio']:checked");
     if(answers.length === numPreguntas) {
-        $(".hidden").removeClass("hidden")
+        answers.parent().parent().find("span.hidden").removeClass("hidden");
     } else {
         alert("Es necesario responder todas las preguntas.")
     }
