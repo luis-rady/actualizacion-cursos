4e130c15a3fa6cd49c1fc499d98f93d64933872b
Luis Manríquez <56151652+LuisMnrqz@users.noreply.github.com>
2020-03-18 13:49:36 -0600

    ejercicio mod1 unidad 3 y correcion de ejercicios seleccion mod1

--- cursos/Uso-de-TICs/app/assets/js/scripts/modulo1/unidad3-11.js
+++ cursos/Uso-de-TICs/app/assets/js/scripts/modulo1/unidad3-11.js
@@ -8,7 +8,7 @@ function getPreguntas() {
             p1.push($(this).attr("value"));
         });
 
-        if (p1.length == 2 && p1[0] == '1' && p1[1] == '3' || p1.length == 2 && p1[0] == '3' && p1[1] == '1') {
+        if (p1.length == 2 && p1[0] == 'Reunir datos del usuario' && p1[1] == 'Investigar las soluciones primero' || p1.length == 2 && p1[0] == 'Investigar las soluciones primero' && p1[1] == 'Reunir datos del usuario') {
             $("#ans1").css("color", "green")
             $("#ans1").text("¡Correcto!").show();
             
@@ -24,7 +24,7 @@ function getPreguntas() {
             p2.push($(this).attr("value"));
         });
 
-        if (p2.length == 2 && p2[0] == '2' && p2[1] == '6' || p2.length == 2 && p2[0] == '6' && p2[1] == '2') {
+        if (p2.length == 2 && p2[0] == 'Probar las resoluciones rápidas de primero' && p2[1] == 'Reunir soluciones con otros usuarios' || p2.length == 2 && p2[0] == 'Reunir soluciones con otros usuarios' && p2[1] == 'Probar las resoluciones rápidas de primero') {
             $("#ans2").css("color", "green")
             $("#ans2").text("¡Correcto!").show();
             
@@ -40,7 +40,7 @@ function getPreguntas() {
             p3.push($(this).attr("value"));
         });
 
-        if (p3.length == 2 && p3[0] == '4' && p3[1] == '5' || p3.length == 2 && p3[0] == '5' && p3[1] == '4') {
+        if (p3.length == 2 && p3[0] == 'Verificar las cuestiones obvias' && p3[1] == 'Reunir datos de la computadora' || p3.length == 2 && p3[0] == 'Reunir datos de la computadora' && p3[1] == 'Verificar las cuestiones obvias') {
             $("#ans3").css("color", "green")
             $("#ans3").text("¡Correcto!").show();
             
@@ -48,6 +48,22 @@ function getPreguntas() {
         else {
             $("#ans3").css("color", "red")
             $("#ans3").text("Incorrecto, intenta de nuevo.").show();
+        }
+
+        // Get pregunta4
+        let p4 = [];
+        $("input[type=checkbox][name=pregunta4]:checked").each(function() {
+            p4.push($(this).attr("value"));
+        });
+
+        if (p4.length == 2 && p4[0] == 'Reunir información de fuentes externas' && p4[1] == 'Evaluar el problema e implementar la solución' || p4.length == 2 && p4[0] == 'Evaluar el problema e implementar la solución' && p4[1] == 'Reunir información de fuentes externas') {
+            $("#ans4").css("color", "green")
+            $("#ans4").text("¡Correcto!").show();
+            
+        }
+        else {
+            $("#ans4").css("color", "red")
+            $("#ans4").text("Incorrecto, intenta de nuevo.").show();
         }
     })
 }
